90bb38d4ec80b9b496820ef36b0b5fac344e0306
Richardrflsn <105922097+Richardrflsn@users.noreply.github.com>
2024-03-31 13:26:27 +0700

    finished project

---
+++ app/src/main/java/com/example/watchme/Activity/IntroActivity.java
@@ -0,0 +1,30 @@
+//Create by Richard Rafelson
+
+package com.example.watchme.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.watchme.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        AppCompatButton getInBtn = findViewById(R.id.getInBtn);
+        getInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
